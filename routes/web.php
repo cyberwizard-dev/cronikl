4480c0450f8e6f35fb34417b8cf127ddba4d3dbd
Cyberwizard <53139650+cyberwizard-dev@users.noreply.github.com>
2025-05-12 16:04:51 +0100

    Route fix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Scheduler;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', \App\Http\Livewire\Scheduler::class);
+Route::get('/', Scheduler::class);
